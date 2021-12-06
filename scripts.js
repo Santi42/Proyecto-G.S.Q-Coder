dafff4f015348c507a6533b12cde6179f94515aa
Santiago Conforti <confortisantiagoe@gmail.com>
2021-12-05 21:38:18 -0300

    Se agrego contenido a las paginas

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+let carouselM = document.querySelectorAll('.m .im')
 
-items.forEach((el) => {
+carouselM.forEach((el) => {
     const minPerSlide = 4
     let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
+    for (var i = 1; i < minPerSlide; i++) {
         if (!next) {
             // wrap carousel by using first child
-        	next = items[0]
-      	}
+            next = carouselM[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+
+let carouselU = document.querySelectorAll('.u .iu')
+
+carouselU.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = carouselU[0]
+        }
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
         next = next.nextElementSibling
